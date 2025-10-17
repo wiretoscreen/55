d9c7bbd0cc4a7b837ff50b1ce665326ab109470f
wiretoscreen <75181938+wiretoscreen@users.noreply.github.com>
2025-10-17 17:27:11 -0300

    Initial Commit: Try to modify the message time TEST

---
+++ plugins/WIPEncryption/src/index.ts
@@ -0,0 +1,95 @@
+import { FluxDispatcher } from "@vendetta/metro/common";
+import { before } from "@vendetta/patcher";
+import { findByProps, findByName } from "@vendetta/metro";
+import { logger } from "@vendetta";
+import { storage } from "@vendetta/plugin";
+import Settings from "./settings";
+
+const RowManager = findByName("RowManager");
+
+const pluginName = "Encryption";
+function constructMessage(message, channel) {
+    let msg = {
+        id: "",
+        type: 0,
+        content: "",
+        channel_id: channel.id,
+        author: {
+            id: "",
+            username: "",
+            avatar: "",
+            discriminator: "",
+            publicFlags: 0,
+            avatarDecoration: null,
+        },
+        attachments: [],
+        embeds: [],
+        mentions: [],
+        mention_roles: [],
+        pinned: false,
+        mention_everyone: false,
+        tts: false,
+        timestamp: "",
+        edited_timestamp: null,
+        flags: 0,
+        components: [],
+    };
+
+    if (typeof message === "string") msg.content = message;
+    else msg = { ...msg, ...message };
+
+    return msg;
+}
+
+const shouldModify = (message) => {
+    return message?.content?.startsWith("<enc:");
+};
+
+let patches = [];
+
+const startPlugin = () => {
+    try {
+        const patch1 = before("generate", RowManager.prototype, ([data]) => {
+            if (shouldModify(data.message)) {
+                const date = new Date(data.message.timestamp);
+                const hours = String(date.getHours()).padStart(2, '0');
+                const minutes = String(date.getMinutes()).padStart(2, '0');
+                const currentTime = `${hours}:${minutes}`;
+
+                data.timestamp.text = `${currentTime} • Test`;
+
+            }
+        });
+        patches.push(patch1);
+
+        logger.log(`${pluginName} loaded.`);
+    } catch (err) {
+        logger.error(`[${pluginName} Error]`, err);
+    }
+};
+
+export default {
+    onLoad: () => {
+        logger.log(`Loading ${pluginName}...`);
+
+        for (let type of ["MESSAGE_CREATE", "MESSAGE_UPDATE", "LOAD_MESSAGES", "LOAD_MESSAGES_SUCCESS"]) {
+            logger.log(`Dispatching ${type} to enable handler.`);
+            FluxDispatcher.dispatch({
+                type,
+                message: constructMessage("PLACEHOLDER", { id: "0" }),
+                messages: [],
+            });
+        }
+        
+        startPlugin();
+    },
+
+    onUnload: () => {
+        logger.log(`Unloading ${pluginName}...`);
+        for (let unpatch of patches) unpatch();
+        patches = [];
+        logger.log(`${pluginName} unloaded.`);
+    },
+
+    settings: Settings,
+};
