541185349441459387ba96f94ab5ee87e811c58f
wiretoscreen <75181938+wiretoscreen@users.noreply.github.com>
2025-10-17 18:08:06 -0300

    another fix

--- plugins/WIPEncryption/src/index.ts
+++ plugins/WIPEncryption/src/index.ts
@@ -59,7 +59,7 @@ const startPlugin = () => {
                 const decryptedContent = (() => { try { return atob(data.message.content.split(":")[1]) } catch { return null } })();
                 if(decryptedContent == null) return
                 data.message.content = `${decryptedContent}\n-# [Triggered Encryption]`;
-            } catch(e) return
+            } catch(e) { return }
             }
         });
         patches.push(patch1);
